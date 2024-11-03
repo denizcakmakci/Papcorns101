b52b1fa80af4caa75dd955f6951057c64c8ca813
Deniz Çakmakçı <dnzckmc@gmail.com>
2024-11-03 15:48:22 +0300

    Initial Commit

---
+++ Papcorns101/Papcorns101App.swift
@@ -0,0 +1,17 @@
+//
+//  Papcorns101App.swift
+//  Papcorns101
+//
+//  Created by Deniz Çakmakçı on 3.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Papcorns101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Papcorns101Tests/Papcorns101Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Papcorns101Tests.swift
+//  Papcorns101Tests
+//
+//  Created by Deniz Çakmakçı on 3.11.2024.
+//
+
+import Testing
+@testable import Papcorns101
+
+struct Papcorns101Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
